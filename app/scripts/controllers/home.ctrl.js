d0c313d0ddb11033ca7ad0d72356dd47f771c875
Syam S Pillai <syam.sp0965@gmail.com>
2016-12-15 14:26:48 +0530

    resolved gulp issues

--- app/scripts/controllers/home.ctrl.js
+++ app/scripts/controllers/home.ctrl.js
@@ -1,24 +1,28 @@
-angular.module('hambreApp')
-  .controller('homeCtrl', ['$scope', 'commonApiService', '$state', function($scope, commonApiService, $state){
-  
-    var home = this;
-    
-    home.init = function(){
-      commonApiService.getCategory()
-    }
-    home.init()
-  
-    home.getlocation = function(){
-      if (navigator.geolocation) {
-        console.log("clicked");
-        navigator.geolocation.getCurrentPosition(function(position){
-          $scope.$apply(function(){
-            $scope.position = position;
-            console.log("location", $scope.position);
-            commonApiService.getGeoCode($scope.position.coords)
-          });
-        });
+(function (){
+
+  angular.module('hambreApp')
+    .controller('homeCtrl', ['$scope', 'commonApiService', '$state', function($scope, commonApiService, $state){
+
+      var home = this;
+
+      home.init = function(){
+        commonApiService.getCategory()
       }
-    }
+      home.init()
+
+      home.getlocation = function(){
+        if (navigator.geolocation) {
+          console.log("clicked");
+          navigator.geolocation.getCurrentPosition(function(position){
+            $scope.$apply(function(){
+              $scope.position = position;
+              console.log("location", $scope.position);
+              commonApiService.getGeoCode($scope.position.coords)
+            });
+          });
+        }
+      }
+
+    }]);
     
-  }])
+})();
